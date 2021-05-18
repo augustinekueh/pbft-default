0cc7e88329d5763415d01b3aa8d41db2e83521fd
Augustine Kueh <augustinekueh@gmail.com>
2021-05-18 21:39:46 +0800

    Draft 3.0; adding batch and executable files

---
+++ group.go
@@ -0,0 +1,29 @@
+package main
+
+/*
+
+var ClientNode 	*KnownNode
+var PrimaryNode	*KnownNode
+var KnownNodes  []*KnownNodes
+var merit 		int
+
+
+type KnownNode struct{
+	nodeID string
+	addr   string
+}
+
+func divideNodes(){
+
+}
+
+func calcTrust(){
+
+}
+
+func findPrimaryG(){
+
+}
+*/
+
+
